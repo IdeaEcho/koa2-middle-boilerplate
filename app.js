c7f219364d732a1289c9c34c0fcb8c1d686eec4a
chenshuyao <chenshuyao@4399inc.com>
2019-09-05 14:02:30 +0800

    docs: 修改项目名称

--- app.js
+++ app.js
@@ -5,19 +5,17 @@ const session = require('koa-session');
 const bodyPaser = require('koa-bodyparser');
 const static = require('koa-static');
 const router = require('./server/middlewares/router/router')(); //路由控制文件
-const redirect = require('./server/middlewares/request/apiRedirect');
-const context = require('./server/middlewares/context/index');
+const redirect = require('./server/middlewares/request/apiRedirect'); //转发接口
+const context = require('./server/middlewares/context/index'); //扩展 context，配置CDN地址
 const cors = require('./server/middlewares/request/cors');
 const error = require('./server/middlewares/error/notfound');
 
 app.use(error)
     .use(cors)
-    //扩展 context，配置CDN地址
     .use(context) 
     .use(bodyPaser({
         formLimit: 1024 * 1024 * 20
     }))
-    //转发接口
     .use(redirect)
     .use(static(__dirname + '/release', {
         maxAge: process.env.NODE_ENV === 'production' ? 60 * 60 * 24 * 30 * 1000 : 0
